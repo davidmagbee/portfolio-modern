7b4429aa2983c28276ac121c310181948a07afbb
“davidmagbee” <david.magbee@me.com>
2024-03-06 15:51:58 -0500

    About content, styling, and motion

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -7,7 +7,12 @@ type Props = {};
 
 export default function About({}: Props) {
   return (
-    <div className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
+    >
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         About
       </h3>
@@ -23,6 +28,20 @@ export default function About({}: Props) {
         className="mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]"
         alt="david on the river"
       />
-    </div>
+      <div className="space-y-10 px-0 md:px-10">
+        <h4 className="text-4xl font-semibold">
+          A <span className="underline decoration-[#468621]/50">bit</span> of
+          background about me...{" "}
+        </h4>
+        <p className="text-base">
+          I am a full-stack developer who approaches projects with humans and
+          purpose in mind. I am passionate about developing efficiencies that
+          improve workflow and create space for creative collaboration and
+          innovation. I am a firm believer in empowering individuals and teams
+          through structured educational guidance so that they may reach
+          personal and professional goals.
+        </p>
+      </div>
+    </motion.div>
   );
 }
