36e2bf26d0d81710065673c4c4e0d7c22c8b3e1d
“davidmagbee” <david.magbee@me.com>
2024-03-06 15:11:27 -0500

    hero image && title

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,15 +3,16 @@
 import React from "react";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 import BackgroundCircles from "./BackgroundCircles";
+import Image from "next/image";
 
 type Props = {};
 
 const Hero = (props: Props) => {
   const [text, count] = useTypewriter({
     words: [
-      "Hello 👋🏼, My Name is David",
-      "Guy-who-loves-coffee.tsx",
-      "<ButLovesToCodeMore />",
+      "Hello, My name is David",
+      "Guy-who-loves-to-code.tsx",
+      "<ButLovesHisDaughterMore />",
     ],
     loop: true,
     delaySpeed: 2000,
@@ -19,6 +20,8 @@ const Hero = (props: Props) => {
   return (
     <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
         <BackgroundCircles />
+        <Image className="relative rounded-full h-32 w-32 mx-auto object-cover" src="/david.png" alt="david" width={128} height={128} />
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">Software Engineer</h2>
       <h1>
         <span>{text}</span>
         <Cursor cursorColor="darkslategray" />
