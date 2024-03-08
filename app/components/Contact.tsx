45cac2a27ec652b5310a0aef58ed9822ac039c43
“davidmagbee” <david.magbee@me.com>
2024-03-08 12:15:16 -0500

    initial contact form mailto setup - todo: incorporate formspree

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -1,9 +1,26 @@
 import { EnvelopeIcon, MapPinIcon, PhoneIcon } from "@heroicons/react/16/solid";
 import React from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type Inputs = {
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
+};
 
 type Props = {};
 
 function Contact({}: Props) {
+  const {
+    register,
+    handleSubmit,
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (formData) => {
+    window.location.href = `mailto:5I8oA@example.com?subject=${formData.subject}&body=Hi, my name is ${formData.name}. ${formData.message} (${formData.email})`;
+  }
+  
   return (
     <div className="h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
@@ -32,15 +49,16 @@ function Contact({}: Props) {
             <p className="text-2xl">Better Farm</p>
           </div>
         </div>
-
-        <form className="flex flex-col space-y-2 w-fit mx-auto">
+        {/* TODO: Update contact form to incorporate formspree from portfolio v2 */}
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col space-y-2 w-fit mx-auto">
           <div className="flex space-x-2">
-            <input placeholder="Name" className="contactInput" type="text" />
-            <input placeholder="Email" className="contactInput" type="email" />
+            <input {...register('name')} placeholder="Name" className="contactInput" type="text" />
+            <input {...register('email')} placeholder="Email" className="contactInput" type="email" />
           </div>
 
-          <input placeholder="Subject" className="contactInput" type="text" />
+          <input {...register('subject')} placeholder="Subject" className="contactInput" type="text" />
           <textarea
+            {...register('message')}
             placeholder="Message"
             className="contactInput"
             name=""
