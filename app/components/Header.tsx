3716c84f89e82f2c1008d58920913ae587400366
“davidmagbee” <david.magbee@me.com>
2024-03-08 12:59:12 -0500

    email icon redirect update to contact form, add hero redirect footer

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,13 +3,14 @@
 import React from "react";
 import { SocialIcon } from "react-social-icons";
 import { motion } from "framer-motion";
+import Link from "next/link";
 
 type Props = {};
 
 const Header = (props: Props) => {
   return (
     <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
-      {/* Social Icons   */}
+      {/* Social Icons | TODO: update social links && + farm/radio icons   */}
       <motion.div
         className="flex flex-row items-center"
         initial={{
@@ -33,27 +34,27 @@ const Header = (props: Props) => {
           bgColor="transparent"
         />
       </motion.div>
-
-      <motion.div
-        className="flex flex-row items-center text-gray-300 cursor-pointer"
-        initial={{
-          x: -500,
-          opacity: 0,
-          scale: 0.5,
-        }}
-        animate={{ x: 0, opacity: 1, scale: 1 }}
-        transition={{ duration: 1 }}
-      >
-        <SocialIcon
-          className="cursor-pointer"
-          network="email"
-          url="mailto:david.magbee@me.com?subject=Let's Connect"
-          bgColor="transparent"
-        />
-        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
-          Let&apos;s Connect
-        </p>
-      </motion.div>
+      <Link href="#contact">
+        <motion.div
+          className="flex flex-row items-center text-gray-300 cursor-pointer"
+          initial={{
+            x: -500,
+            opacity: 0,
+            scale: 0.5,
+          }}
+          animate={{ x: 0, opacity: 1, scale: 1 }}
+          transition={{ duration: 1 }}
+        >
+          <SocialIcon
+            className="cursor-pointer"
+            network="email"
+            bgColor="transparent"
+          />
+          <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+            Let&apos;s Connect
+          </p>
+        </motion.div>
+      </Link>
     </header>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Experience from "./components/Experience";
@@ -5,6 +6,7 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -34,6 +36,20 @@ export default function Home() {
       <section id="contact" className="snap-start">
         <Contact />
       </section>
+
+      <Link href="#hero">
+        <footer className="sticky bottom-5 w-full cursor-pointer">
+          <div className="flex items-center justify-center">
+            <Image
+              className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer"
+              src="/david-pointing.png"
+              alt="david"
+              width={128}
+              height={128}
+            />
+          </div>
+        </footer>
+      </Link>
     </main>
   );
 }
